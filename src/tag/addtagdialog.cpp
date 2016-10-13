66fb9b8a048afabbaaedbc863b742eee6ecaa6c6
Montel Laurent <montel@kde.org>
2016-10-13 13:46:08 +0200

    Fix crash when we open dialog

--- src/tag/addtagdialog.cpp
+++ src/tag/addtagdialog.cpp
@@ -63,8 +63,8 @@ AddTagDialog::AddTagDialog(const QList<KActionCollection *> &actions, QWidget *p
     connect(d->mTagWidget->tagNameLineEdit(), &KLineEdit::textChanged, this, &AddTagDialog::slotTagNameChanged);
 
     QDialogButtonBox *buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
-    d->mOkButton->setShortcut(Qt::CTRL | Qt::Key_Return);
     d->mOkButton = buttonBox->button(QDialogButtonBox::Ok);
+    d->mOkButton->setShortcut(Qt::CTRL | Qt::Key_Return);
     connect(buttonBox, &QDialogButtonBox::accepted, this, &AddTagDialog::slotSave);
     connect(buttonBox, &QDialogButtonBox::rejected, this, &AddTagDialog::reject);
     d->mOkButton->setDefault(true);
