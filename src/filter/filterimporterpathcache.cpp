b3c39df257bf8cf64796dfa41d191d0a5f7c8c29
Montel Laurent <montel@kde.org>
2017-10-26 21:15:58 +0200

    Start to create a cache for path when we import filter

---
+++ src/filter/filterimporterpathcache.cpp
@@ -0,0 +1,51 @@
+/*
+  Copyright (c) 2017 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "filterimporterpathcache.h"
+
+using namespace MailCommon;
+FilterImporterPathCache::FilterImporterPathCache(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+FilterImporterPathCache::~FilterImporterPathCache()
+{
+
+}
+
+FilterImporterPathCache *FilterImporterPathCache::self()
+{
+    static FilterImporterPathCache s_self;
+    return &s_self;
+}
+
+void FilterImporterPathCache::insert(const QString &original, const QString &newValue)
+{
+    mFilterCache.insert(original, newValue);
+}
+
+QString FilterImporterPathCache::convertedFilterPath(const QString &original)
+{
+    return mFilterCache.value(original);
+}
+
+void FilterImporterPathCache::clear()
+{
+    mFilterCache.clear();
+}
---
+++ src/filter/filterimporterpathcache.h
@@ -0,0 +1,42 @@
+/*
+  Copyright (c) 2017 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef FILTERIMPORTERPATHCACHE_H
+#define FILTERIMPORTERPATHCACHE_H
+
+#include <QObject>
+#include <QHash>
+namespace MailCommon
+{
+class FilterImporterPathCache : public QObject
+{
+    Q_OBJECT
+public:    
+    static FilterImporterPathCache *self();
+
+    explicit FilterImporterPathCache(QObject *parent = nullptr);
+    ~FilterImporterPathCache();
+
+    void insert(const QString &original, const QString &newValue);
+    QString convertedFilterPath(const QString &original);
+    void clear();
+private:
+    QHash<QString, QString> mFilterCache;
+};
+}
+
+#endif // FILTERIMPORTERPATHCACHE_H
