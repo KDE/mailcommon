85c0a1e74f2fd3cf121c58779d712684b39244b0
Laurent Montel <montel@kde.org>
2020-09-21 13:45:00 +0200

    Move class here. It will be use by kdepim-addons plugins

---
+++ src/collectionpage/collectiontemplateswidget.h
@@ -0,0 +1,42 @@
+/*
+   SPDX-FileCopyrightText: 2020 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#ifndef COLLECTIONTEMPLATESWIDGET_H
+#define COLLECTIONTEMPLATESWIDGET_H
+
+#include "mailcommon_export.h"
+#include <QWidget>
+#include <AkonadiCore/Collection>
+class QCheckBox;
+namespace TemplateParser {
+class TemplatesConfiguration;
+}
+
+namespace MailCommon {
+/**
+ * @brief The CollectionTemplateWidget class
+ * @author Laurent Montel <montel@kde.org>
+ */
+class MAILCOMMON_EXPORT CollectionTemplatesWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit CollectionTemplatesWidget(QWidget *parent = nullptr);
+    ~CollectionTemplatesWidget();
+    void save(const Akonadi::Collection &);
+    void load(const Akonadi::Collection &col);
+private:
+    void slotCopyGlobal();
+    void slotChanged();
+    QCheckBox *mCustom = nullptr;
+    TemplateParser::TemplatesConfiguration *mWidget = nullptr;
+    QString mCollectionId;
+    uint mIdentity = 0;
+    bool mChanged = false;
+};
+}
+
+#endif // COLLECTIONTEMPLATESWIDGET_H
