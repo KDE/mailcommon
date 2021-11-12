de9c5f286dc5418747b19be27e0d65926cd5fab1
Ingo Klöcker <kloecker@kde.org>
2021-11-12 14:27:42 +0100

    Add some documentation for the HierarchicalFolderMatcher
    This explains how the matching works, gives a few examples, and lists
    design considerations.
    
    Funded by: Intevation GmbH
    
    CCBUG: 443791
    CCMAIL: bernhard@intevation.de

--- src/folder/hierarchicalfoldermatcher_p.h
+++ src/folder/hierarchicalfoldermatcher_p.h
@@ -18,6 +18,40 @@ class QString;
 
 namespace MailCommon
 {
+/* This class allows filtering a hierarchical item model with path-like
+ * patterns like "parent/sub" similar to file path globbing.
+ *
+ * The matching works as follows:
+ * * The pattern is split into sub-patterns wherever "/" occurs.
+ * * The sub-patterns are matched against each item and its ancestors.
+ * * The pattern matches for an item if all sub-patterns match the item and
+ *   its ancestors.
+ * * The sub-pattern matching uses wildcard matching with implied "*" before
+ *   and after each sub-pattern. For details see
+ *   QRegularExpression::wildcardToRegularExpression().
+ *
+ * Examples:
+ * * "foo" matches all items that contain "foo".
+ * * "foo/bar" matches all items matching "bar" with parent items matching "foo".
+ * * "foo/" matches all child items of items matching "foo".
+ * * "/bar" matches all items matching "bar" which have a parent item.
+ * * "foo//bar" matches all items matching "bar" with grandparents matching "foo".
+ *
+ * For more examples have a look at the tests in foldertreewidgettest.cpp.
+ *
+ * Design decisions:
+ * * The original request was that "foo" should match items that contain "foo"
+ *   *and* their (direct?) child items, so that one can easily narrow down the
+ *   list of folders to the folders belonging to a subfolder tree, e.g. the
+ *   subfolder tree for "project foo".
+ *   I decided against this for the following reasons:
+ *   * The requested result can be achieved with "foo/".
+ *   * If there are multiple folders matching "foo" with a larger number of
+ *     subfolders, then it would not have been easy to select the wanted
+ *     folder matching "foo" because of all the subfolders that are also listed.
+ *   * I also think that the behavior of the original request isn't really
+ *     intuitive, at least for people who know file path globbing.
+ */
 class HierarchicalFolderMatcher
 {
 public:
