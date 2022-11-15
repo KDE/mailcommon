6f9bdac8aa2bd3b1cd93d226da7b9142368caeec
Laurent Montel <montel@kde.org>
2022-11-15 07:58:31 +0100

    Now we depend against kf5.100

--- src/snippets/snippetsmodel.cpp
+++ src/snippets/snippetsmodel.cpp
@@ -17,7 +17,6 @@
 #include <QIODevice>
 #include <QMimeData>
 #include <QStringList>
-#include <kwidgetsaddons_version.h>
 
 using namespace MailCommon;
 
@@ -522,17 +521,12 @@ bool SnippetsModel::dropMimeData(const QMimeData *data, Qt::DropAction action, i
             Q_EMIT dndDone();
             return true;
         } else {
-#if KWIDGETSADDONS_VERSION >= QT_VERSION_CHECK(5, 100, 0)
             if (KMessageBox::ButtonCode::PrimaryAction
                 == KMessageBox::questionTwoActions(nullptr,
                                                    i18n("Do you want to update snippet?"),
                                                    i18n("Update snippet"),
                                                    KGuiItem(i18n("Update")),
                                                    KStandardGuiItem::cancel())) {
-#else
-            if (KMessageBox::Yes
-                == KMessageBox::questionYesNo(nullptr, i18n("Do you want to update snippet?"), i18n("Update snippet"), KGuiItem(i18n("Update")))) {
-#endif
                 item->setText(text);
                 item->setSubject(subject);
                 item->setTo(to);
@@ -556,16 +550,12 @@ bool SnippetsModel::dropMimeData(const QMimeData *data, Qt::DropAction action, i
         if (item->isGroup()) {
             Q_EMIT addNewDndSnippset(encodedData);
         } else {
-#if KWIDGETSADDONS_VERSION >= QT_VERSION_CHECK(5, 100, 0)
             if (KMessageBox::ButtonCode::PrimaryAction
                 == KMessageBox::questionTwoActions(nullptr,
                                                    i18n("Do you want to update snippet?"),
                                                    i18n("Update snippet"),
                                                    KGuiItem(i18n("Update")),
                                                    KStandardGuiItem::cancel())) {
-#else
-            if (KMessageBox::Yes == KMessageBox::questionYesNo(nullptr, i18n("Do you want to update snippet?"), i18n("Update snippet"))) {
-#endif
                 item->setText(encodedData);
             }
         }
