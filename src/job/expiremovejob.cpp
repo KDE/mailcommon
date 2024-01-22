fe90b0251ec277f8954d412fa465349ec8f40067
Laurent Montel <montel@kde.org>
2024-01-22 22:25:13 +0100

    continue to implement expiremovejob

--- src/job/expiremovejob.cpp
+++ src/job/expiremovejob.cpp
@@ -5,6 +5,7 @@
  */
 
 #include "expiremovejob.h"
+#include <Akonadi/ItemMoveJob>
 
 ExpireMoveJob::ExpireMoveJob(QObject *parent)
     : QObject{parent}
@@ -12,3 +13,20 @@ ExpireMoveJob::ExpireMoveJob(QObject *parent)
 }
 
 ExpireMoveJob::~ExpireMoveJob() = default;
+
+Akonadi::Item::List ExpireMoveJob::removedMsgs() const
+{
+    return mRemovedMsgs;
+}
+
+void ExpireMoveJob::setRemovedMsgs(const Akonadi::Item::List &newRemovedMsgs)
+{
+    mRemovedMsgs = newRemovedMsgs;
+}
+
+void ExpireMoveJob::start()
+{
+    // TODO
+}
+
+#include "moc_expiremovejob.cpp"
